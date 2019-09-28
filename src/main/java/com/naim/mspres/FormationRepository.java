71ad7b37607283d32058dfee6fc9c38c093f6e66
Naim <n_bouhalit@esi.dz>
2019-09-28 20:04:18 +0100

    repository

--- src/main/java/com/naim/mspres/FormationRepository.java
+++ src/main/java/com/naim/mspres/FormationRepository.java
@@ -2,7 +2,7 @@ package com.naim.mspres;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
+//@CrossOrigin("*")
 @RepositoryRestResource
 public interface FormationRepository extends JpaRepository<Formation,Long> {
 }
---
+++ src/main/java/com/naim/mspres/GlobalRepositoryRestConfigurer.java
@@ -0,0 +1,20 @@
+package com.naim.mspres;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class GlobalRepositoryRestConfigurer extends RepositoryRestConfigurerAdapter {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration repositoryRestConfiguration) {
+        repositoryRestConfiguration.setReturnBodyOnCreate(true);
+        repositoryRestConfiguration.setReturnBodyOnUpdate(true);
+        repositoryRestConfiguration.exposeIdsFor(Etudiant.class,Formation.class);
+        repositoryRestConfiguration.getCorsRegistry()
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("OPTIONS","GET","HEAD","PUT","POST","DELETE","PATCH");
+    }
+}
